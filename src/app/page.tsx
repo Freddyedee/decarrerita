d914519a69726c62124ebcf151fc9e864c58321c
Freddyedee <freddyaranguiz26@gmail.com>
2026-07-24 00:36:33 +0000

    Fix: Forzar renderizado dinamico en la pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { listUsers } from '@modules/user/application/listUsers';
 
+export const dynamic = "force-dynamic";
 export default async function HomePage() {
   const users = await listUsers();
 
