f7fa59e67303efda91973b806aab16712c1a58ad
Freddyedee <freddyaranguiz26@gmail.com>
2026-07-24 02:03:34 +0000

    Fix: corregir dependencias y parámetros en UserRouter

--- src/modules/user/presentation/router/UserRouter.ts
+++ src/modules/user/presentation/router/UserRouter.ts
@@ -1,7 +1,6 @@
 import { UserController } from "../controllers/UserController";
 
 import { PrismaUserRepository } from "../../infrastructure/repositories/PrismaUserRepository";
-// 1. IMPORTAMOS LOS NUEVOS REPOSITORIOS Y EL MANAGER
 import { PrismaClientRepository } from "../../infrastructure/repositories/PrismaClientRepository";
 import { PrismaDriverRepository } from "../../infrastructure/repositories/PrismaDriverRepository";
 import { BancoRepository } from "@/modules/banco/infrastructure/prisma/BancoRepository";
@@ -11,16 +10,20 @@ import { CreateUserUseCase } from "../../application/use-cases/CreateUserUseCase
 import { GetUserByIdUseCase } from "../../application/use-cases/GetUserByIdUseCase";
 import { UpdateUserStatusUseCase } from "../../application/use-cases/UpdateUserStatusUseCase";
 import { UpdateUserProfileUseCase } from "../../application/use-cases/UpdateUserProfileUseCase";
-import { prisma } from "@/infra/prisma/client";
+// 1. IMPORTAMOS EL CASO DE USO FALTANTE
+import { GetAllUsersUseCase } from "../../application/use-cases/GetAllUsersUseCase"; 
 
-// 2. INSTANCIAMOS TODO PASÁNDOLE PRISMA
+// 2. CORREGIMOS LA RUTA DE PRISMA (Usando la de tu proyecto)
+import { prisma } from "@/shared/lib/prisma"; 
+
+// 3. INSTANCIAMOS TODO PASÁNDOLE PRISMA
 const userRepository = new PrismaUserRepository(prisma);
 const clientRepository = new PrismaClientRepository(prisma);
 const driverRepository = new PrismaDriverRepository(prisma);
 const transactionManager = new PrismaTransactionManager(); 
-const bancoRepository = new BancoRepository();
+const bancoRepository = new BancoRepository(prisma);
 
-// 3. SE LOS PASAMOS AL CASO DE USO EN EL ORDEN EXACTO DEL CONSTRUCTOR
+// 4. SE LOS PASAMOS AL CASO DE USO EN EL ORDEN EXACTO
 const createUserUseCase = new CreateUserUseCase(
     userRepository,
     clientRepository,
@@ -32,12 +35,13 @@ const createUserUseCase = new CreateUserUseCase(
 const getUserByIdUseCase = new GetUserByIdUseCase(userRepository);
 const updateUserProfileUseCase = new UpdateUserProfileUseCase(userRepository);
 const updateUserStatusUseCase = new UpdateUserStatusUseCase(userRepository);
-
-/////////////////////////////////////////////////////////////
+// 5. INSTANCIAMOS EL CASO DE USO FALTANTE
+const getAllUsersUseCase = new GetAllUsersUseCase(userRepository); 
 
 export const userController = new UserController(
     createUserUseCase,
     getUserByIdUseCase,
     updateUserProfileUseCase,
     updateUserStatusUseCase,
+    getAllUsersUseCase // 6. SE LO PASAMOS AL CONTROLADOR
 );
